81792c78408100bcdabdd8cfc7093f6751778dbb
jeffkillian <jeffkillian@gmail.com>
2018-11-10 15:43:19 -0800

    Working Snap your fingers

--- musicbox.ino
+++ musicbox.ino
@@ -16,11 +16,13 @@ int readValue;
 
 const int buttonOnePin = 2;     // the number of the pushbutton pin
 const int buttonTwoPin = 3;     // the number of the pushbutton pin
+const int buttonThreePin = 4;
 
 void setup() {
     // initialize the pushbutton pin as an input:
   pinMode(buttonOnePin, INPUT);
   pinMode(buttonTwoPin, INPUT);
+  pinMode(buttonThreePin, INPUT);
     Serial.begin(9600);      // turn on Serial Port
 }
 
@@ -28,15 +30,20 @@ void loop() {
   // read the state of the pushbutton value:
   int buttonOneState = digitalRead(buttonOnePin);
    int buttonTwoState = digitalRead(buttonTwoPin);
+    int buttonThreeState = digitalRead(buttonThreePin);
   Serial.println(buttonTwoState);
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonOneState == HIGH) {
-     tone(8, 300, 50);
-     delay(200);
+     tone(8, NOTE_GS5, 50);
+     delay(60);
   }
     if (buttonTwoState == HIGH) {
-     tone(8, 3000, 50);
-     delay(200);
+     tone(8, NOTE_AS5, 50);
+     delay(60);
+  }
+    if (buttonThreeState == HIGH) {
+     tone(8, NOTE_B5, 50);
+     delay(60);
   }
 }
 
