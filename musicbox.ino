5d760a1dd23e678ff6f2c01ecc711c4e0c662d0a
jeffkillian <jeffkillian@gmail.com>
2018-11-10 16:20:11 -0800

    Snap your fingers two octaves

--- musicbox.ino
+++ musicbox.ino
@@ -1,14 +1,14 @@
 /*
-CoMotion MakerSpace 2018
-Edited by: Ramon Qu
-Arduino 101 Basic Workshop
+  CoMotion MakerSpace 2018
+  Edited by: Ramon Qu
+  Arduino 101 Basic Workshop
   12 - Melody
- Plays a melody
- circuit:
- * 8-ohm speaker on digital pin 8
- This example code is in the public domain.
- http://www.arduino.cc/en/Tutorial/Tone
- */
+  Plays a melody
+  circuit:
+   8-ohm speaker on digital pin 8
+  This example code is in the public domain.
+  http://www.arduino.cc/en/Tutorial/Tone
+*/
 #include "pitches.h"
 int writeValue;
 int readValue;
@@ -17,36 +17,43 @@ int readValue;
 const int buttonOnePin = 2;     // the number of the pushbutton pin
 const int buttonTwoPin = 3;     // the number of the pushbutton pin
 const int buttonThreePin = 4;
+const int buttonFourPin = 5;
 
+int notes[2][3] = {{NOTE_GS5, NOTE_AS5, NOTE_B5},
+{NOTE_GS7, NOTE_AS7, NOTE_B7}};
 void setup() {
-    // initialize the pushbutton pin as an input:
+  // initialize the pushbutton pin as an input:
   pinMode(buttonOnePin, INPUT);
   pinMode(buttonTwoPin, INPUT);
   pinMode(buttonThreePin, INPUT);
-    Serial.begin(9600);      // turn on Serial Port
+  pinMode(buttonFourPin, INPUT);
+  Serial.begin(9600);      // turn on Serial Port
 }
 
 void loop() {
   // read the state of the pushbutton value:
   int buttonOneState = digitalRead(buttonOnePin);
-   int buttonTwoState = digitalRead(buttonTwoPin);
-    int buttonThreeState = digitalRead(buttonThreePin);
-  Serial.println(buttonTwoState);
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  int buttonTwoState = digitalRead(buttonTwoPin);
+  int buttonThreeState = digitalRead(buttonThreePin);
+  int buttonFourState = digitalRead(buttonFourPin);
+  int noteIndex = 0;
   if (buttonOneState == HIGH) {
-     tone(8, NOTE_GS5, 50);
-     delay(60);
+    noteIndex = 1;
   }
-    if (buttonTwoState == HIGH) {
-     tone(8, NOTE_AS5, 50);
-     delay(60);
+  if (buttonTwoState == HIGH) {
+    tone(8, notes[noteIndex][0], 50);
+    delay(60);
   }
-    if (buttonThreeState == HIGH) {
-     tone(8, NOTE_B5, 50);
-     delay(60);
+  if (buttonThreeState == HIGH) {
+    tone(8, notes[noteIndex][1], 50);
+    delay(60);
+  }
+  if (buttonFourState == HIGH) {
+    tone(8, notes[noteIndex][2], 50);
+    delay(60);
   }
 }
 
 /*
-Try to create your own song.
+  Try to create your own song.
 */
